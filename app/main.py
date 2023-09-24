4012de205b63deabfef30bc1005c49e36a64f9f9
kozi6666 <kubakoko63@gmail.com>
2023-09-24 18:05:46 +0200

    add foods

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -31,6 +33,25 @@ async def get_current_user():
 @app.get("/users/{user_id}")
 async def get_item(user_id: str):
     return {"user_id": user_id}
+
+
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    dairy = "dairy"
+
+
+@app.get("/foods/{food_name}")
+async def get_food(food_name: FoodEnum):
+    if food_name == FoodEnum.vegetables:
+        return {"food_name": food_name, "message": "you are healthy"}
+
+    if food_name.value == "fruits":
+        return {
+            "food_name": food_name,
+            "message": "you are still healthy, but like sweet things ",
+        }
+    return {"food_name": food_name, "message": "i like chockolate milk"}
 
 
 # TODO add items router in new python package "routers" and pydantic item model
